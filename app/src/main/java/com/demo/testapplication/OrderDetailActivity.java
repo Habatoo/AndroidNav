e276acb398f8be689435215a0ab945e76df0c16d
Habatoo <habatoo@gmail.com>
2020-10-13 20:13:50 +0700

    complete project

--- app/src/main/java/com/demo/testapplication/OrderDetailActivity.java
+++ app/src/main/java/com/demo/testapplication/OrderDetailActivity.java
@@ -2,13 +2,27 @@ package com.demo.testapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class OrderDetailActivity extends AppCompatActivity {
+
+    private TextView textViewReceivedOrder;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_order_detail);
+        textViewReceivedOrder = findViewById(R.id.textViewReceivedOrder);
+
+        Intent intent = getIntent();
+        if (intent.hasExtra("fullOrder")) {
+            String fullOrder = intent.getStringExtra("fullOrder");
+            textViewReceivedOrder.setText(fullOrder);
+        } else {
+            Intent backToLogin = new Intent(this, CafeProjectActivity.class);
+            startActivity(backToLogin);
+        }
     }
 }
