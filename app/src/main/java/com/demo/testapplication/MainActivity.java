71a54b428e45b0e8c45f33c1ffa8a6c174048a1e
Habatoo <habatoo@gmail.com>
2020-10-13 21:04:46 +0700

    add drill shop project

--- app/src/main/java/com/demo/testapplication/MainActivity.java
+++ app/src/main/java/com/demo/testapplication/MainActivity.java
@@ -58,4 +58,9 @@ public class MainActivity extends AppCompatActivity {
         Intent intent = new Intent(this, CafeProjectActivity.class);
         startActivity(intent);
     }
+
+    public void startShopProject(View view) {
+        Intent intent = new Intent(this, ShopProjectActivity.class);
+        startActivity(intent);
+    }
 }
---
+++ app/src/main/java/com/demo/testapplication/ShopProjectActivity.java
@@ -0,0 +1,44 @@
+package com.demo.testapplication;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.Toast;
+
+public class ShopProjectActivity extends AppCompatActivity {
+
+    private ListView listViewTools;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_shop_project);
+
+        // hide ActionBar
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.hide();
+        }
+
+        listViewTools = findViewById(R.id.listViewTools);
+        // add listener - find area of list that was click
+        listViewTools.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                // Toast.makeText(getApplicationContext(), "Позиция: " + position, Toast.LENGTH_SHORT).show();
+                switch (position) {
+                    case 0:
+                        Intent intent = new Intent(getApplicationContext(), DrillActivity.class);
+                        startActivity(intent);
+                        break;
+                }
+                Toast.makeText(getApplicationContext(), "Позиция: " + position, Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
