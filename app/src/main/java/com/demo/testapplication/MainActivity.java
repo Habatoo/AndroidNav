81ec218ce0d86c86aba40995c2abc2c9aa13248c
Habatoo <habatoo@gmail.com>
2020-10-14 20:04:03 +0700

    change main UI

--- app/src/main/java/com/demo/testapplication/MainActivity.java
+++ app/src/main/java/com/demo/testapplication/MainActivity.java
@@ -1,66 +1,78 @@
 package com.demo.testapplication;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ListView listViewProjects;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
 
-    public void onClickEnter(View view) {
-        Intent intent = new Intent(this, EnterActivity.class);
-        startActivity(intent);
-    }
+        // hide ActionBar
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.hide();
+        }
 
-    public void onClickFirst(View view) {
-        Intent intent = new Intent(this, FirstActivity.class);
-        startActivity(intent);
-    }
-
-    public void onClickSecond(View view) {
-        Intent intent = new Intent(this, SecondActivity.class);
-        startActivity(intent);
-    }
-
-    public void onClickSpinner(View view) {
-        Intent intent = new Intent(this, SpinnerActivity.class);
-        startActivity(intent);
-    }
-
-    public void newIntend(View view) {
-        Intent intent = new Intent(this, CreateMessageActivity.class);
-        startActivity(intent);
-    }
-
-    public void selectAndroidSend(View view) {
-        Intent intent = new Intent(this, DiffMessageActivity.class);
-        startActivity(intent);
-    }
-
-    public void startWatch(View view) {
-        Intent intent = new Intent(this, WatchActivity.class);
-        startActivity(intent);
-    }
-
-    public void addingStartWatch(View view) {
-        Intent intent = new Intent(this, WatchActivityAddActivity.class);
-        startActivity(intent);
-    }
-
-    public void startCafeProject(View view) {
-        Intent intent = new Intent(this, CafeProjectActivity.class);
-        startActivity(intent);
-    }
-
-    public void startShopProject(View view) {
-        Intent intent = new Intent(this, ShopProjectActivity.class);
-        startActivity(intent);
+        listViewProjects = findViewById(R.id.listViewProjects);
+        // add listener - find area of list that was click
+        listViewProjects.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                switch (position) {
+                    case 0:
+                        Intent enter = new Intent(getApplicationContext(), EnterActivity.class);
+                        startActivity(enter);
+                        break;
+                    case 1:
+                        Intent first = new Intent(getApplicationContext(), FirstActivity.class);
+                        startActivity(first);
+                        break;
+                    case 2:
+                        Intent second = new Intent(getApplicationContext(), SecondActivity.class);
+                        startActivity(second);
+                        break;
+                    case 3:
+                        Intent spinner = new Intent(getApplicationContext(), SpinnerActivity.class);
+                        startActivity(spinner);
+                        break;
+                    case 4:
+                        Intent messenger = new Intent(getApplicationContext(), CreateMessageActivity.class);
+                        startActivity(messenger);
+                        break;
+                    case 5:
+                        Intent messengerAndroid = new Intent(getApplicationContext(), DiffMessageActivity.class);
+                        startActivity(messengerAndroid);
+                        break;
+                    case 6:
+                        Intent watch = new Intent(getApplicationContext(), WatchActivity.class);
+                        startActivity(watch);
+                        break;
+                    case 7:
+                        Intent watchRotate = new Intent(getApplicationContext(), WatchActivityAddActivity.class);
+                        startActivity(watchRotate);
+                        break;
+                    case 8:
+                        Intent cafe = new Intent(getApplicationContext(), CafeProjectActivity.class);
+                        startActivity(cafe);
+                        break;
+                    case 9:
+                        Intent shop = new Intent(getApplicationContext(), ShopProjectActivity.class);
+                        startActivity(shop);
+                        break;
+                }
+            }
+        });
     }
 }
