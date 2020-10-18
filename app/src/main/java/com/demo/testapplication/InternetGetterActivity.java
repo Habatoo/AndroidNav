225597b1e9727f6e454ac53bbdf6c546f53c74a6
Habatoo <habatoo@gmail.com>
2020-10-18 14:41:33 +0700

    add download image project

--- app/src/main/java/com/demo/testapplication/InternetGetterActivity.java
+++ app/src/main/java/com/demo/testapplication/InternetGetterActivity.java
@@ -1,10 +1,16 @@
 package com.demo.testapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.AsyncTask;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -18,6 +24,9 @@ import java.util.concurrent.ExecutionException;
 public class InternetGetterActivity extends AppCompatActivity {
 
     private  String mailRu = "https://mail.ru";
+    private TextView textViewTextShow;
+    private ImageView imageViewShowImage;
+    private String imageUrl = "https://avatars3.githubusercontent.com/u/34543104?s=460&v=4";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,17 +36,61 @@ public class InternetGetterActivity extends AppCompatActivity {
         // send info to logger using key and value
         //Log.i("Hello", "Hello World");
         //Log.i("mailRu", mailRu);
+        textViewTextShow = findViewById(R.id.textViewTextShow);
+        imageViewShowImage = findViewById(R.id.imageViewShowImage);
 
+    }
+
+    public void showText(View view) {
         // make new thread
         DownloadTask task = new DownloadTask();
         // make try catch - for data from internet
         try {
             String result = task.execute(mailRu).get();
             Log.i("URL", result);
+            textViewTextShow.setText(result);
         } catch (ExecutionException e) {
             e.printStackTrace();
         } catch (InterruptedException e) {
             e.printStackTrace();
+        }
+    }
+
+    public void showImage(View view) {
+        DownloadImageTask downloadTask = new DownloadImageTask();
+        Bitmap bitmap = null;
+        try {
+            bitmap = downloadTask.execute(imageUrl).get();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        imageViewShowImage.setImageBitmap(bitmap);
+    }
+
+    private static class DownloadImageTask extends AsyncTask<String, Void, Bitmap> {
+        @Override
+        protected Bitmap doInBackground(String... strings) {
+            URL imageUrl = null;
+            HttpURLConnection urlConnection = null;
+
+            try {
+                imageUrl = new URL(strings[0]);
+                urlConnection = (HttpURLConnection) imageUrl.openConnection();
+                InputStream inputStream = urlConnection.getInputStream();
+                Bitmap bitmap = BitmapFactory.decodeStream(inputStream);
+                return bitmap;
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+               if (urlConnection != null) {
+                   urlConnection.disconnect();
+               }
+            }
+            return null;
         }
     }
 
